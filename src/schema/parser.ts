b0c885f6178127ead9f8a03b3bc43946dcbb8088
Heber Senger <sengerhe@gmail.com>
2025-06-07 07:03:34 -0300

    parse schema

--- src/schema/parser.ts
+++ src/schema/parser.ts
@@ -1,17 +1,95 @@
-export function parseAndValidateSchema(schema: string) {
+export interface Config {
+  schemas: string[];
+  queries: string[];
+}
+
+export function parseAndValidateConfig(schema: string): {
+  parsedConfig: Config | null;
+  errors: string[];
+} {
   const parsedSchema = JSON.parse(schema);
   const errors: string[] = [];
 
   if (!Array.isArray(parsedSchema.schemas)) {
-    errors.push("sqld schema invalid: schemas must be an array");
+    errors.push("sqld config invalid: schemas must be an array");
   } else if (parsedSchema.schemas.length === 0) {
-    errors.push("sqld schema invalid: schemas must not be empty");
+    errors.push("sqld config invalid: schemas must not be empty");
   }
 
   if (!Array.isArray(parsedSchema.queries)) {
-    errors.push("sqld schema invalid: queries must be an array");
+    errors.push("sqld config invalid: queries must be an array");
   } else if (parsedSchema.queries.length === 0) {
-    errors.push("sqld schema invalid: queries must not be empty");
+    errors.push("sqld config invalid: queries must not be empty");
+  }
+
+  if (errors.length > 0) {
+    return { parsedConfig: null, errors };
+  }
+
+  return { parsedConfig: parsedSchema, errors: [] };
+}
+
+interface Schema {
+  tables: Table[];
+}
+
+interface Table {
+  name: string;
+  columns: Column[];
+}
+
+interface Column {
+  name: string;
+  type: string;
+}
+
+export function parseAndValidateSchema(schema: string): {
+  parsedSchema: Schema | null;
+  errors: string[];
+} {
+  const errors: string[] = [];
+
+  const parsedSchema: Schema = {
+    tables: [],
+  };
+
+  const createTableRegex = /CREATE TABLE (\w+)\s*\(([\s\S]*?)\);/gim;
+  const matches = schema.matchAll(createTableRegex);
+
+  for (const match of matches) {
+    if (!match) {
+      errors.push("sqld schema invalid: invalid schema content");
+      break;
+    }
+
+    const tableName = match[1];
+    const columns = match[2].split(",").map((column) => column.trim());
+
+    const columnRegex = /(\w+)\s+([\w\(\)]+)/;
+    const columnMatches = columns
+      .map((column) => column.match(columnRegex))
+      .filter((column) => column !== null);
+
+    parsedSchema.tables.push({
+      name: tableName,
+      columns: columnMatches.map((column) => ({
+        name: column[1],
+        type: column[2],
+      })),
+    });
+  }
+
+  if (parsedSchema.tables.length === 0) {
+    errors.push("sqld schema invalid: no tables found");
+  }
+
+  const countCreateTableStatements = schema.match(/CREATE TABLE/g)?.length;
+  if (countCreateTableStatements !== parsedSchema.tables.length) {
+    errors.push("sqld schema invalid: tables count does not match");
+  }
+
+  if (errors.length > 0) {
+    return { parsedSchema: null, errors };
   }
 
   return { parsedSchema, errors };
--- src/schema/schema.spec.ts
+++ src/schema/schema.spec.ts
@@ -1,18 +1,85 @@
 import { describe, it } from "node:test";
 import assert from "assert";
-import { parseAndValidateSchema } from "./parser";
+import { parseAndValidateConfig, parseAndValidateSchema } from "./parser";
 
-describe("schema", () => {
-  it("validates schema object", () => {
-    const schema = `{
+describe("config parser", () => {
+  it("validates config object", () => {
+    const config = `{
         "schemas": ["schema.sql"],
         "queries": ["queries.sql"]
     }`;
 
-    const { parsedSchema, errors } = parseAndValidateSchema(schema);
+    const { parsedConfig, errors } = parseAndValidateConfig(config);
 
     assert(errors.length === 0);
-    assert.equal(parsedSchema.schemas.length, 1);
-    assert.equal(parsedSchema.queries.length, 1);
+    assert.equal(parsedConfig?.schemas.length, 1);
+    assert.equal(parsedConfig?.queries.length, 1);
+  });
+
+  it("validates config object with invalid schemas", () => {
+    const config = `{
+      "schemas": "schema.sql",
+      "queries": "queries.sql"
+    }`;
+
+    const { parsedConfig, errors } = parseAndValidateConfig(config);
+
+    assert(errors.length === 2);
+    assert.equal(errors[0], "sqld config invalid: schemas must be an array");
+    assert.equal(errors[1], "sqld config invalid: queries must be an array");
+    assert.equal(parsedConfig, null);
+  });
+});
+
+describe("schema parser", () => {
+  it("parses schema", () => {
+    const schema = `
+        CREATE TABLE users (
+            id INT PRIMARY KEY,
+            name VARCHAR(255)
+        );
+
+        CREATE TABLE posts (
+            id INT PRIMARY KEY,
+            title VARCHAR(255),
+            content TEXT
+        );
+    `;
+
+    const { parsedSchema, errors } = parseAndValidateSchema(schema);
+
+    assert.equal(errors.length, 0, "errors is not empty: " + errors.join(", "));
+    assert.equal(parsedSchema?.tables.length, 2);
+
+    assert.equal(parsedSchema?.tables[0].name, "users");
+    assert.equal(parsedSchema?.tables[0].columns.length, 2);
+    assert.equal(parsedSchema?.tables[0].columns[0].name, "id");
+    assert.equal(parsedSchema?.tables[0].columns[0].type, "INT");
+    assert.equal(parsedSchema?.tables[0].columns[1].name, "name");
+    assert.equal(parsedSchema?.tables[0].columns[1].type, "VARCHAR(255)");
+
+    assert.equal(parsedSchema?.tables[1].name, "posts");
+    assert.equal(parsedSchema?.tables[1].columns.length, 3);
+    assert.equal(parsedSchema?.tables[1].columns[0].name, "id");
+    assert.equal(parsedSchema?.tables[1].columns[0].type, "INT");
+    assert.equal(parsedSchema?.tables[1].columns[1].name, "title");
+    assert.equal(parsedSchema?.tables[1].columns[1].type, "VARCHAR(255)");
+  });
+
+  it("detects invalid create table statements", () => {
+    const schema = `
+        CREATE TABLE users (
+            id INT PRIMARY KEY,
+            name VARCHAR(255)
+        );
+
+        CREATE TABLE posts ()
+    `;
+
+    const { parsedSchema, errors } = parseAndValidateSchema(schema);
+
+    assert.equal(errors.length, 1);
+    assert.equal(errors[0], "sqld schema invalid: tables count does not match");
+    assert.equal(parsedSchema, null);
   });
 });
